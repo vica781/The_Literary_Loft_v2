17427fc72ffad27b65b59492894b7fe3e3644cb6
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-07-27 12:27:46 +0100

    Style: add style to register page

--- reading_loft/static/js/view_password.js
+++ reading_loft/static/js/view_password.js
@@ -8,3 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.toggle('fa-eye-slash');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
+    const confirmPassword = document.getElementById('confirm_password');
+
+    toggleConfirmPassword.addEventListener('click', function() {
+        const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+        confirmPassword.setAttribute('type', type);
+        this.classList.toggle('fa-eye-slash');
+    });
+});
