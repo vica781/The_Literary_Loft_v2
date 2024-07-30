76740a93a695a3d463c887d0726d11946dda4f06
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-07-30 14:53:14 +0100

    Admin: register Category, Subcategory and Book models in admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Subcategory, Book
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Book)
