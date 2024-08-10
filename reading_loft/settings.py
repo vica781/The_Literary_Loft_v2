b07c4d7d4c0d7490f80afe31f5a1e64d5587c744
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-08-10 18:36:09 +0100

    Remov secret key and set debug

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xn=s!-(cj3*+bih7)(9c148_xw2z0x1b8yg#1(2!de0fy6@*he'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', '127.0.0.1']
 
