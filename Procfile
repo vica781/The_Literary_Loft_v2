cb6d043cc2ba86f53bb9773d13f1dcae5222932a
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-08-10 17:44:04 +0100

    First attempt to deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn reading_loft.wsgi:application
--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-xn=s!-(cj3*+bih7)(9c148_xw2z0x1b8yg#1(2!de0fy6@*he
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-reading-loft.herokuapp.com', ' localhost']
 
 # Application definition
 
@@ -112,13 +113,17 @@ WSGI_APPLICATION = 'reading_loft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:       
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
