454b886ae465bb01a1fc6a21e17a320d5cda3972
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-08-12 19:34:07 +0100

    Reinstall packages without secrets

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -13,6 +13,9 @@ import os
 import dj_database_url
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
