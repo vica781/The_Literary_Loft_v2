7939305355f9dbb77c81c26ac3490e6112606765
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-08-12 19:50:08 +0100

    Update load_dotenv in settings.py

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv('DEVELOPMENT'):
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
