0ddd5d76c103e9f292b5aef5c5abc3a42dc956e8
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-08-12 19:58:37 +0100

    Update ALLOWED_HOSTS setting

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['the-literary-loft-a4b6116b3a17.herokuapp.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 INSTALLED_APPS = [
