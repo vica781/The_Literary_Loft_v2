90d95657b6bdabc26bb6408468a2d9d7a20d768f
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-08-12 20:31:22 +0100

    Fix: loading env vars

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
 
-if os.getenv('DEVELOPMENT'):
-    load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
