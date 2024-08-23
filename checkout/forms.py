bd8405adfded514431877939859a8dafb0675d18
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-08-23 15:28:17 +0100

    Feat: add and style Stripe elements

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,6 +7,17 @@ class OrderForm(forms.ModelForm):
         fields = ['full_name', 'email', 'phone_number', 
                   'country', 'postcode', 'town_or_city', 
                   'street_address1', 'street_address2', 'county']
+        widgets = {
+            'full_name': forms.TextInput(attrs={'id': 'full_name', 'name': 'full_name'}),
+            'email': forms.EmailInput(attrs={'id': 'email', 'name': 'email'}),
+            'phone_number': forms.TextInput(attrs={'id': 'phone_number', 'name': 'phone_number'}),
+            'country': forms.TextInput(attrs={'id': 'country', 'name': 'country'}),
+            'postcode': forms.TextInput(attrs={'id': 'postcode', 'name': 'postcode'}),
+            'town_or_city': forms.TextInput(attrs={'id': 'town_or_city', 'name': 'town_or_city'}),
+            'street_address1': forms.TextInput(attrs={'id': 'street_address1', 'name': 'street_address1'}),
+            'street_address2': forms.TextInput(attrs={'id': 'street_address2', 'name': 'street_address2'}),
+            'county': forms.TextInput(attrs={'id': 'county', 'name': 'county'}),
+        }
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- checkout/static/js/checkout.js
+++ checkout/static/js/checkout.js
@@ -1,37 +1,21 @@
-src="https://js.stripe.com/v3/"
+let form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    let stripe = Stripe('{{ stripe_public_key }}');
-    let elements = stripe.elements();
-    let card = elements.create('card');
-    card.mount('#card-element');
-
-    card.on('change', function(event) {
-        let displayError = document.getElementById('card-errors');
-        if (event.error) {
-            displayError.textContent = event.error.message;
+    stripe.confirmCardPayment("{{ client_secret }}", {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // Show error to your customer
+            let displayError = document.getElementById('card-errors');
+            displayError.textContent = result.error.message;
         } else {
-            displayError.textContent = '';
+            if (result.paymentIntent.status === 'succeeded') {
+                // The payment has been processed!
+                window.location.href = "{% url 'books:order_success' %}";
+            }
         }
     });
-
-    let form = document.getElementById('payment-form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        stripe.confirmCardPayment("{{ client_secret }}", {
-            payment_method: {
-                card: card,
-            }
-        }).then(function(result) {
-            if (result.error) {
-                // Show error to your customer
-                let displayError = document.getElementById('card-errors');
-                displayError.textContent = result.error.message;
-            } else {
-                if (result.paymentIntent.status === 'succeeded') {
-                    // The payment has been processed!
-                    window.location.href = "{% url 'books:order_success' %}";
-                }
-            }
-        });
-    });
+});
