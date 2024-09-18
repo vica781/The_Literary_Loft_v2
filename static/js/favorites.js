5b0f8001668a0fd4b5cd825074c85c9b49419a00
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-09-18 22:46:27 +0100

    Feat: improve favorite books functionality and style

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -29,10 +29,15 @@ $(document).ready(function() {
         }
     });
 
-    $('.favorite-btn').click(function(e) {
+    function updateFavoriteCount(count) {
+        $('.favorites-count').text(count);
+    }
+
+    function handleFavoriteClick(e) {
         e.preventDefault();
-        var bookId = $(this).data('book-id');
         var $btn = $(this);
+        var bookId = $btn.data('book-id');
+        var isOnFavoritesPage = $btn.closest('.favorite-book-item').length > 0;
 
         $.ajax({
             url: '/books/toggle-favorite/' + bookId + '/',
@@ -42,6 +47,9 @@ $(document).ready(function() {
                     $btn.addClass('active');
                 } else {
                     $btn.removeClass('active');
+                    if (isOnFavoritesPage) {
+                        $btn.closest('.favorite-book-item').remove();
+                    }
                 }
                 updateFavoriteCount(data.favorite_count);
             },
@@ -49,9 +57,16 @@ $(document).ready(function() {
                 console.error("AJAX error: " + textStatus + ' : ' + errorThrown);
             }
         });
-    });
+    }
 
-    function updateFavoriteCount(count) {
-        $('#favorite-count').text(count);
+    // Use event delegation to handle clicks on favorite buttons
+    $(document).on('click', '.favorite-btn', handleFavoriteClick);
+
+    // Check if we're on the favorites page
+    if ($('#favorites-container').length > 0) {
+        // If there are no favorite books, show a message
+        if ($('.favorite-book-item').length === 0) {
+            $('#favorites-container').html('<p class="nunito">You haven\'t added any books to your favorites yet.</p>');
+        }
     }
 });
