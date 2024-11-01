7be4797d4f69b01e0ba11c0d7d0094d4e0d67a58
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-11-01 16:18:11 +0000

    Fix: relocate 404.html

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-literary-loft-a4b6116b3a17.herokuapp.com', 'localhost', '127.0.0.1']
 
