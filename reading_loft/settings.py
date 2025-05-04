bdfb4088d89a88ad9410274e1efc13e16a046834
Victoria Kalenik-Harris <vk781@hotmail.com>
2025-05-04 19:53:07 +0100

    Revert: switch back to SMTP email backend for live password reset

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -115,9 +115,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Email Settings for Gmail
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
