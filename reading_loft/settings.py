c45f0996387320151026b176aa76c6ae03f89460
Victoria Kalenik-Harris <vk781@hotmail.com>
2025-05-04 19:26:03 +0100

    Fix: Temporarily switch to console email backend

--- reading_loft/settings.py
+++ reading_loft/settings.py
@@ -115,7 +115,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Email Settings for Gmail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
