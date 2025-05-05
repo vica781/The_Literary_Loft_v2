be106130919b213114f22b838da62901f7f1f792
Victoria Kalenik-Harris <vk781@hotmail.com>
2025-05-05 14:34:44 +0100

    fix: Uncomment UserProfile model to enable profile functionality

--- checkout/models.py
+++ checkout/models.py
@@ -59,26 +59,28 @@ class OrderItem(models.Model):
     def __str__(self):
         return f'{self.quantity} of {self.book.title}'
 
-# # USER PROFILE INFORMATION
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-#     default_full_name = models.CharField(max_length=50, null=True, blank=True)
-#     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-#     default_country = models.CharField(max_length=40, null=True, blank=True)
-#     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-#     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-#     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-#     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-#     default_county = models.CharField(max_length=80, null=True, blank=True)
+# USER PROFILE INFORMATION
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    default_full_name = models.CharField(max_length=50, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = models.CharField(max_length=40, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
 
-#     def __str__(self):
-#         return self.user.username
+    def __str__(self):
+        return self.user.username
 
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# @receiver(post_save, sender=User)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Only save if the profile exists to prevent errors during migration
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
